72cacc6dbee4f85649f911d8eff05b01735dc736
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-08-06 09:23:04 +0900

    README修正、最小化

--- src/author.model.ts
+++ src/author.model.ts
@@ -2,7 +2,7 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Author {
-  @Field((type) => Int)
+  @Field(() => Int)
   id: number;
 
   @Field({ nullable: true })
--- src/authors.resolver.ts
+++ src/authors.resolver.ts
@@ -2,7 +2,7 @@ import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 
 import { Author } from './author.model';
 
-@Resolver((of) => Author)
+@Resolver(() => Author)
 export class AuthorResolver {
   readonly authorMaps = new Map<number, Author>([
     [1, { id: 1, firstName: 'hige', lastName: 'hoge' }],
@@ -10,13 +10,8 @@ export class AuthorResolver {
     [3, { id: 3, firstName: 'sample', lastName: 'hoge' }],
   ]);
 
-  @Query((returns) => Author)
+  @Query(() => Author)
   async author(@Args('id', { type: () => Int }) id: number) {
-    const author = this.authorMaps.get(id);
-    return {
-      id,
-      firstName: author.firstName,
-      lastName: author.lastName,
-    };
+    return this.authorMaps.get(id);
   }
 }
